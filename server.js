16346642e4b7578db6af37ee2709db8347203c56
toxin <yuri.belousov.1998@gmail.com>
2018-12-10 17:10:41 +0300

    hmmmmmm

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 4200);
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/memastick-front'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/memastick-front/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
