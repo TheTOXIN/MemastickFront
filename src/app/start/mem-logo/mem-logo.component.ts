afc84c88a3380c99fe629c2baddb8ed00dc79122
toxin <yuri.belousov.1998@gmail.com>
2018-12-28 16:47:36 +0300

    + random hello message

--- src/app/start/mem-logo/mem-logo.component.ts
+++ src/app/start/mem-logo/mem-logo.component.ts
@@ -7,7 +7,18 @@ import {Component, OnInit} from '@angular/core';
 })
 export class MemLogoComponent implements OnInit {
 
+  public helloRandom: string;
+
+  private helloMessages = [
+    'Кажется тебе пора эволюционировать!',
+    'Переходи на нашу сторону, у нас есть мемасики...',
+    'Не знаю как ты сюда попал, но ты там где тебе нужно',
+    'Добро пожаловать! Тебе у нас понравится :)',
+    'Псс... эй... да ты, мемов не хочешь?'
+  ];
+
   constructor() {
+    this.helloRandom = this.helloMessages[Math.floor(Math.random() * this.helloMessages.length)];
   }
 
   ngOnInit() {
