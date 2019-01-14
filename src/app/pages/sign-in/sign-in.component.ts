80d894b07282f513a43026ff73c3473e9aff63a4
toxin <yuri.belousov.1998@gmail.com>
2019-01-14 19:03:35 +0300

    + sign in logic

--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,10 +8,49 @@ import {Component, OnInit} from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() {
+  private messages = [
+    'Мы уже соскучились по тебе :)',
+    'Быстрее возвращайся!',
+    'Ну давай залезай сюда',
+    'Где тебя носило? Мы волновались',
+    'Вы только посмотрите кто пожаловал!'
+  ];
+
+  public message: String;
+  public error = false;
+
+  public signForm: FormGroup;
+  public signType: String = 'login';
+
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.signForm = new FormGroup({});
+    this.message = this.messages[Math.floor(Math.random() * this.messages.length)];
   }
 
   ngOnInit() {
+    this.signForm = this.fb.group({
+      password: ['', Validators.required],
+      email: [],
+      login: [],
+    });
+  }
+
+  onSubmit() {
+    if (
+      this.signForm.value.email == null || this.signForm.value.email === '' &&
+      this.signForm.value.password == null || this.signForm.value.password === ''
+    ) {
+      this.error = true;
+      this.message = 'Введите логин или почту!';
+    }
+
+    console.log(
+      this.signForm.value.login,
+      this.signForm.value.email,
+      this.signForm.value.password
+    );
   }
 
 }
