28e7b69d89899e90394c572fa737a602212fee67
toxin <thetoksin@gmail.com>
2019-01-15 07:08:18 +0300

    + logic sign-up

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,10 +8,44 @@ import {Component, OnInit} from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() {
+  private messages = [
+    'Милости прошу к нашему шалашу',
+    'Быстрее возвращайся!',
+    'Ну давай залезай сюда',
+    'Где тебя носило? Мы волновались',
+    'Вы только посмотрите кто пожаловал!'
+  ];
+
+  public message: String;
+  public error = false;
+
+  public signForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.signForm = new FormGroup({});
+    this.message = this.messages[Math.floor(Math.random() * this.messages.length)];
   }
 
   ngOnInit() {
+    this.signForm = this.fb.group({
+      password: ['', Validators.required],
+      passwordRepeat: ['', Validators.required],
+      email: ['', Validators.required],
+      login: ['', Validators.required],
+      invite: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    console.log(
+      this.signForm.value.login,
+      this.signForm.value.email,
+      this.signForm.value.password,
+      this.signForm.value.passwordRepeat,
+      this.signForm.value.invite,
+    );
   }
 
 }
