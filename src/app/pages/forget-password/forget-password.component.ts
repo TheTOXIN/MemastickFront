a8557a632f1ae26bfa6a6bfa49ccdabd8508dc24
toxin <thetoksin@gmail.com>
2019-01-15 07:56:04 +0300

    + logic forget password

--- src/app/pages/forget-password/forget-password.component.ts
+++ src/app/pages/forget-password/forget-password.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-forget-password',
@@ -7,10 +8,62 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ForgetPasswordComponent implements OnInit {
 
-  constructor() {
+  private messages = [
+    'Мы отправим тебе код для восстановления пароля',
+    'Подтверди код восстановления ',
+    'Введите новый пароль',
+    'Постарайся больше не забывать пароли :)',
+  ];
+
+  public message: String;
+  public error = false;
+  public stepNumber = 0;
+
+  public sendForm: FormGroup;
+  public acceptForm: FormGroup;
+  public changeForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.sendForm = new FormGroup({});
+    this.acceptForm = new FormGroup({});
+    this.changeForm = new FormGroup({});
+
+    this.nextStep();
   }
 
   ngOnInit() {
+    this.sendForm = this.fb.group({
+      email: ['', Validators.email],
+    });
+    this.acceptForm = this.fb.group({
+      code: ['', Validators.required],
+    });
+    this.changeForm = this.fb.group({
+      password: ['', Validators.required],
+      passwordRepeat: ['', Validators.required],
+    });
+  }
+
+  onSend() {
+    console.log(this.sendForm.value.email);
+    this.nextStep();
+  }
+
+  onAccept() {
+    console.log(this.acceptForm.value.code);
+    this.nextStep();
+  }
+
+  onChange() {
+    console.log(this.changeForm.value.password, this.changeForm.value.passwordRepeat);
+    this.nextStep();
+  }
+
+  nextStep() {
+    this.stepNumber++;
+    this.message = this.messages[this.stepNumber - 1];
   }
 
 }
