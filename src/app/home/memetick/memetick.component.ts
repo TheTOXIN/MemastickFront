3e1b13ca47835890604fcea029e3926be2ed31d2
toxin <thetoksin@gmail.com>
2019-01-26 20:08:57 +0300

    + memetick profile design

--- src/app/home/memetick/memetick.component.ts
+++ src/app/home/memetick/memetick.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MemetickApiService} from '../../services/memetick-api-service';
+import {Memetick} from '../../model/Memetick';
 
 @Component({
   selector: 'app-memetick',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemetickComponent implements OnInit {
 
-  constructor() { }
+  public avatarURL: String = 'dasdasdasdasd';
+
+  public memetick: Memetick = new Memetick(
+    'asdasdasdasd',
+    'SUKA BLYAT'
+  );
+
+  constructor(
+    private memetickApi: MemetickApiService,
+  ) {
+
+  }
 
   ngOnInit() {
+    this.memetickApi.me().subscribe(data => {
+      this.memetick = data;
+    });
   }
 
 }
