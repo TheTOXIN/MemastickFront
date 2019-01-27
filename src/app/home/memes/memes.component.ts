45ca5b0d9d7c351dbdb8ed168a89388497ffd5c7
toxin <thetoksin@gmail.com>
2019-01-27 21:18:29 +0300

    f css

--- src/app/home/memes/memes.component.ts
+++ src/app/home/memes/memes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {PaginationService} from '../../services/pagination-service';
+import {Memetick} from '../../model/Memetick';
 
 @Component({
   selector: 'app-memes',
@@ -7,6 +8,33 @@ import {PaginationService} from '../../services/pagination-service';
   styleUrls: ['./memes.component.scss']
 })
 export class MemesComponent implements OnInit {
+
+  public avatarURL: String = 'dasdasdasdasd';
+
+  public memes = [
+    'assets/memes/1.jpg',
+    'assets/memes/2.jpg',
+    'assets/memes/3.jpg',
+    'assets/memes/4.jpg',
+    'assets/memes/5.jpg',
+    'assets/memes/6.jpg',
+    'assets/memes/7.jpg',
+    'assets/memes/8.jpg',
+    'assets/memes/9.jpg',
+    'assets/memes/10.jpg',
+  ];
+
+  public memInfo = {
+    chromosome: 100500,
+    like: 777,
+    meLike: false,
+    myChromosome: 20
+  };
+
+  public memetick: Memetick = new Memetick(
+    'blablalbal',
+    'TEST_NICK'
+  );
 
   constructor(
     public page: PaginationService
