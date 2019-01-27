54d50ff03e53873856674af1b0f5ff413cd034f1
toxin <thetoksin@gmail.com>
2019-01-27 22:27:51 +0300

    + memes

--- src/app/home/memes/memes.component.ts
+++ src/app/home/memes/memes.component.ts
@@ -1,13 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PaginationService} from '../../services/pagination-service';
 import {Memetick} from '../../model/Memetick';
+import {animate, animation, keyframes, state, style, transition, trigger, useAnimation} from '@angular/animations';
+
 
 @Component({
   selector: 'app-memes',
   templateUrl: './memes.component.html',
-  styleUrls: ['./memes.component.scss']
+  styleUrls: ['./memes.component.scss'],
+  animations: [
+    trigger('rotatedState', [
+      transition('* => *', [
+        style({transform: 'rotate(-180deg)'}),
+        animate('400ms ease-out')
+      ])
+    ]),
+    trigger('bouncedState', [
+      transition('* => *', [
+        style({ transform: 'scale(1)' }),
+        animate(200, keyframes([
+            style({ transform: 'scale(1)', offset: 0 }),
+            style({ transform: 'scale(1.5)', offset: 0.5 }),
+            style({ transform: 'scale(1)', offset: 1 })
+        ]))
+      ])
+    ])
+  ]
 })
 export class MemesComponent implements OnInit {
+
+  chromosomeState = 'default';
+  likeState = 'default';
 
   public avatarURL: String = 'dasdasdasdasd';
 
@@ -43,13 +66,28 @@ export class MemesComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.page.init('memes', 'date', { reverse: true, prepend: false });
+    this.page.init('memes', 'date', {reverse: true, prepend: false});
   }
 
   scrollHandler(e) {
     if (e === 'bottom') {
       this.page.more();
     }
+  }
+
+  triggerChromosome() {
+    if (this.memInfo.myChromosome >= 30) {
+      return;
+    }
+    this.memInfo.chromosome++;
+    this.memInfo.myChromosome++;
+    this.chromosomeState = (this.chromosomeState === 'default' ? 'rotated' : 'default');
+  }
+
+  triggerLike() {
+    this.memInfo.meLike = !this.memInfo.meLike;
+    this.memInfo.like = this.memInfo.meLike ? this.memInfo.like + 1 : this.memInfo.like - 1;
+    this.likeState = (this.likeState === 'default' ? 'bounced' : 'default');
   }
 
 }
