c4158a4ac98b63b888a021f20be21d7fef45c14e
toxin <yuri.belousov.1998@gmail.com>
2019-02-01 11:37:35 +0300

    f memes tmp

--- src/app/home/memes/memes.component.ts
+++ src/app/home/memes/memes.component.ts
@@ -1,13 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {MemesPaginationService} from '../../services/memes-pagination.service';
-import {Memetick} from '../../model/Memetick';
 import {animate, keyframes, style, transition, trigger} from '@angular/animations';
 import {DomSanitizer} from '@angular/platform-browser';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {ImageViewModalComponent} from '../../shared/image-view-modal/image-view-modal.component';
-import {UUID} from 'angular2-uuid';
-import {MemetickAvatarApiService} from '../../services/memetick-avatar-api-service';
 import {MemeApiService} from '../../services/meme-api-service';
+import {MemeLikeApiService} from '../../services/meme-like-api-service';
 
 
 @Component({
@@ -38,21 +36,9 @@ export class MemesComponent implements OnInit {
   chromosomeState = 'default';
   likeState = 'default';
 
-  public memInfo = {
-    chromosome: 100500,
-    like: 777,
-    meLike: false,
-    myChromosome: 20
-  };
-
-  public memetick: Memetick = new Memetick(
-    'blablalbal',
-    'TEST_NICK'
-  );
-
   constructor(
     public page: MemesPaginationService,
-    public avatarApi: MemetickAvatarApiService,
+    private likeApi: MemeLikeApiService,
     private memeApi: MemeApiService,
     private _sanitizer: DomSanitizer,
     private modalService: NgbModal
@@ -61,7 +47,7 @@ export class MemesComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.page.init(1, 5, 'creating', true);
+    this.page.init(1, 3, 'creating', true);
   }
 
   scrollHandler(e) {
@@ -71,17 +57,15 @@ export class MemesComponent implements OnInit {
   }
 
   triggerChromosome() {
-    if (this.memInfo.myChromosome >= 30) {
-      return;
-    }
-    this.memInfo.chromosome++;
-    this.memInfo.myChromosome++;
+    // if (this.memInfo.myChromosome >= 30) {return;}
+    // this.memInfo.chromosome++;
+    // this.memInfo.myChromosome++;
     this.chromosomeState = (this.chromosomeState === 'default' ? 'rotated' : 'default');
   }
 
   triggerLike() {
-    this.memInfo.meLike = !this.memInfo.meLike;
-    this.memInfo.like = this.memInfo.meLike ? this.memInfo.like + 1 : this.memInfo.like - 1;
+    // this.memInfo.meLike = !this.memInfo.meLike;
+    // this.memInfo.like = this.memInfo.meLike ? this.memInfo.like + 1 : this.memInfo.like - 1;
     this.likeState = (this.likeState === 'default' ? 'bounced' : 'default');
   }
 
