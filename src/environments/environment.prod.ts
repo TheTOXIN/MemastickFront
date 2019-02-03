5377c05fa7cb825771ffab4f23441c01eed0794f
toxin <thetoksin@gmail.com>
2019-02-03 10:19:25 +0300

    prod fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '',
-    authDomain: 'memastick-test.firebaseapp.com',
-    databaseURL: 'https://memastick-test.firebaseio.com',
-    projectId: 'memastick-test',
-    storageBucket: 'memastick-test.appspot.com',
-    messagingSenderId: ''
+    apiKey: 'AIzaSyDTkeBmdNaZzFkFxYVUjJeCZQ0Vg9qbHT8',
+    authDomain: 'memastick-d525f.firebaseapp.com',
+    databaseURL: 'https://memastick-d525f.firebaseio.com',
+    projectId: 'memastick-d525f',
+    storageBucket: 'memastick-d525f.appspot.com',
+    messagingSenderId: '1004879436852'
   }
 };
