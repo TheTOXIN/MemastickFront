24a314a4097dc82285ec2ac49988697ddba51b02
toxin <yuri.belousov.1998@gmail.com>
2019-03-21 13:57:55 +0300

    + log refresher

--- src/app/configs/my-http-interceptor.ts
+++ src/app/configs/my-http-interceptor.ts
@@ -5,14 +5,13 @@ import {
   HttpRequest,
 } from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
 import {API} from '../consts/API';
 import {OauthApiService} from '../services/oauth-api-service';
 import {catchError, filter, finalize, switchMap, take} from 'rxjs/operators';
 import {throwError} from 'rxjs';
-
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 import {flatMap} from 'rxjs/internal/operators';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class MyHttpInterceptor implements HttpInterceptor {
@@ -50,6 +49,7 @@ export class MyHttpInterceptor implements HttpInterceptor {
     return next.handle(req).pipe(
       catchError(error => {
         if (error.status === 401) {
+          console.log('REFRESHER');
           return this.refresher(req, next);
         } else {
           return throwError(error);
@@ -58,25 +58,30 @@ export class MyHttpInterceptor implements HttpInterceptor {
     );
   }
 
-  private refresher(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  private refresher(req: HttpRequest<any>, next: HttpHandler): Observable<any> {
     if (!this.isRefreshingToken) {
+      console.log('IS_REFRESH');
       this.isRefreshingToken = true;
       this.tokenSubject.next(null);
       return this.oauthApi.refresh().pipe(
         flatMap((data: any) => {
+          console.log('REFRESH_MAP');
           this.tokenSubject.next(data.access_token);
           this.oauthApi.saveToken(data);
           return next.handle(this.oauthApi.addAuthorization(req, data.access_token));
         }),
         catchError(err => {
+          console.log('REFRESH_ERROR');
           this.oauthApi.logout();
           return throwError(err);
         }),
         finalize(() => {
+          console.log('REFRESH FINALIZE');
           this.isRefreshingToken = false;
         })
       );
     } else {
+      console.log('NOT_REFRESH');
       this.isRefreshingToken = false;
       return this.tokenSubject.pipe(
         filter(token => token != null),
