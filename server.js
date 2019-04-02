c2bc77e8a13ac83ddd5ad5f4d0bc93729d6710af
TheTOXIN <thetoksin@gmail.com>
2019-04-02 21:49:50 +0300

    + ssl redirect heroku

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 //Install express server
 const express = require('express');
+const sslRedirect = require('heroku-ssl-redirect');
 const path = require('path');
-
 const app = express();
+
+//User redirect to https for heroku
+app.use(sslRedirect());
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/frontmem'));
 
+//WTF???
 app.get('/*', function (req, res) {
-
   res.sendFile(path.join(__dirname + '/dist/frontmem/index.html'));
 });
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,6 @@ import {environment} from '../environments/environment';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  ngOnInit(): void {
-    if (environment.production) {
-      if (location.protocol === 'http:') {
-        window.location.href = location.href.replace('http', 'https');
-      }
-    }
-  }
 }
