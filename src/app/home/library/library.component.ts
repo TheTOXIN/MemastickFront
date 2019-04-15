80ce998ea7304dd4daeabe51ede054a78b153e16
toxin <yuri.belousov.1998@gmail.com>
2019-04-15 16:02:38 +0300

    f close button

--- src/app/home/library/library.component.ts
+++ src/app/home/library/library.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-library',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibraryComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  back() {
+    this.router.navigate(['/home']);
+  }
 }
