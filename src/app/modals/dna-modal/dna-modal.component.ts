0effbdf19baf349fecfa71b7997d255bfab877ba
toxin <yuri.belousov.1998@gmail.com>
2019-07-11 18:51:12 +0300

    + dna modal info

--- src/app/modals/dna-modal/dna-modal.component.ts
+++ src/app/modals/dna-modal/dna-modal.component.ts
@@ -8,6 +8,18 @@ import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 })
 export class DnaModalComponent implements OnInit {
 
+  public data = [{
+      text: 'Создание мема',
+      count: '100-1000',
+    },{
+      text: 'Использование токена',
+      count: 'asdasd',
+    },{
+      text: 'За хромосомы',
+      count: '1-10',
+    }
+  ];
+
   constructor(
     public activeModal: NgbActiveModal
   ) {
