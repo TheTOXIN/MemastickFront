d1e2e8ad26107872c6bb744e5a49c3c14b2e38ac
toxin <yuri.belousov.1998@gmail.com>
2019-08-15 18:48:40 +0300

    + main shop

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -5,6 +5,14 @@ import {LoaderStatus} from '../consts/LoaderStatus';
 import {ErrorCode} from '../consts/ErrorCode';
 import {Router} from '@angular/router';
 import {LandingFixService} from '../shared/services/landing-fix.service';
+import {MemeFilter} from '../consts/MemeFilter';
+import {UUID} from 'angular2-uuid';
+
+interface Product {
+  txt: string;
+  url: string;
+  img: string;
+}
 
 @Component({
   selector: 'app-shop',
@@ -13,20 +21,59 @@ import {LandingFixService} from '../shared/services/landing-fix.service';
 })
 export class ShopComponent {
 
+  public products: Product[] = [{
+    txt: 'ПЕЧЕНЬКИ',
+    url: '/cookies',
+    img: '/assets/images/icon/cookie.png',
+  }, {
+    txt: 'МЕМОТИПЫ',
+    url: '/memotypes',
+    img: '/assets/images/icon/cell.png',
+  }, {
+    txt: 'ПОСОБИЕ',
+    url: '/allowance',
+    img: '/assets/images/icon/allowance.png',
+  }, {
+    txt: 'ГРАНТЫ',
+    url: '/grants',
+    img: '/assets/images/icon/books.png',
+  }, {
+    txt: 'КИРКИ',
+    url: '/pickaxe',
+    img: '/assets/images/icon/pickaxe.png',
+  }, {
+    txt: 'ВОСКРЕШЕНИЕ',
+    url: '/resurrection',
+    img: '/assets/images/icon/dead.png',
+  }, {
+    txt: 'НИКНЕЙМ',
+    url: '/nick',
+    img: '/assets/images/tokens/tmp.png',
+  }, {
+    txt: 'ПУБЛИКАЦИЯ',
+    url: '/publish',
+    img: '/assets/images/icon/community.png',
+  }];
+
+  isMain: boolean;
+
   constructor(
     private router: Router
   ) {
-  }
-
-  toCookies() {
-    this.redirect('cookies');
+    this.isMain = this.router.url === '/shop';
   }
 
   redirect(url: string) {
-    this.router.navigateByUrl('/shop/' + url);
+    this.router.navigateByUrl('/shop' + url);
+    this.isMain = false;
   }
 
   back() {
+    this.router.navigateByUrl('/shop');
+    this.isMain = true;
+  }
+
+  close() {
     window.history.back();
   }
 }
