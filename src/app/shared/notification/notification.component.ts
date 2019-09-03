486e5e5f3024e7505667e7d92cb973cec519e9dc
toxin <yuri.belousov.1998@gmail.com>
2019-09-03 11:49:42 +0300

    + battle notify

--- src/app/shared/notification/notification.component.ts
+++ src/app/shared/notification/notification.component.ts
@@ -2,10 +2,8 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {NotifyType} from '../../consts/NotifyType';
 import {Notify} from '../../model/Notify';
 import { timer } from 'rxjs';
-import {TokenType} from '../../consts/TokenType';
 import {MemetickAvatarApiService} from '../../api/memetick-avatar-api-service';
-import {Router} from '@angular/router';
-import {tokenIcons, tokensData} from '../../model/TokenData';
+import {tokenIcons} from '../../model/TokenData';
 
 @Component({
   selector: 'app-notification',
@@ -20,18 +18,43 @@ export class NotificationComponent implements OnInit {
   public img: string;
   public txt: string;
   public inf: string;
-
   public url: string;
 
-  private icons = [];
-
   private audio = new Audio();
+
+  private notifyImg: any[] = [];
+  private notifyInf: any[] = [];
+
+  private tokenIcons = [];
 
   constructor(
     private avatarApi: MemetickAvatarApiService
   ) {
-    this.icons = tokenIcons;
     this.audio.src = '../../../assets/audio/nice.wav';
+
+    this.notifyInf[NotifyType.DNA] = null;
+    this.notifyInf[NotifyType.CELL] = '!';
+    this.notifyInf[NotifyType.TOKEN] = 'T';
+    this.notifyInf[NotifyType.CREATING] = 'NEW';
+    this.notifyInf[NotifyType.ALLOWANCE] = '+';
+    this.notifyInf[NotifyType.ADMIN] = '!!!';
+    this.notifyInf[NotifyType.MEME_COIN] = null;
+    this.notifyInf[NotifyType.BATTLE_REQUEST] = '->';
+    this.notifyInf[NotifyType.BATTLE_RESPONSE] = '<-';
+    this.notifyInf[NotifyType.BATTLE_COMPLETE] = 'END';
+
+    this.notifyImg[NotifyType.DNA] = 'assets/images/icon/3.png';
+    this.notifyImg[NotifyType.CELL] = 'assets/images/icon/cell.png';
+    this.notifyImg[NotifyType.TOKEN] = null;
+    this.notifyImg[NotifyType.CREATING] = null;
+    this.notifyImg[NotifyType.ALLOWANCE] = 'assets/images/icon/allowance.png';
+    this.notifyImg[NotifyType.ADMIN] = null;
+    this.notifyImg[NotifyType.MEME_COIN] = 'assets/images/icon/memecoin.png';
+    this.notifyImg[NotifyType.BATTLE_REQUEST] = 'assets/images/icon/sword.png';
+    this.notifyImg[NotifyType.BATTLE_RESPONSE] = 'assets/images/icon/shield.png';
+    this.notifyImg[NotifyType.BATTLE_COMPLETE] = 'assets/images/icon/battle.png';
+
+    this.tokenIcons = tokenIcons;
   }
 
   ngOnInit(): void {
@@ -52,28 +75,21 @@ export class NotificationComponent implements OnInit {
     this.txt = notify.title;
     this.url = notify.event;
 
-    // TODO ICON FOR BATTLES
-    if (notify.type === NotifyType.DNA) {
-      this.img = 'assets/images/icon/3.png';
+    this.inf = this.notifyInf[notify.type];
+    this.img = this.notifyImg[notify.type];
+
+    if (this.inf == null) {
       this.inf = notify.data;
-    } else if (notify.type === NotifyType.CELL) {
-      this.img = 'assets/images/icon/cell.png';
-      this.inf = '!';
-    } else if (notify.type === NotifyType.TOKEN) {
-      this.img = this.icons[notify.data];
-      this.inf = '';
-    } else if (notify.type === NotifyType.CREATING) {
-      this.img = this.avatarApi.dowloadAvatar(notify.data);
-      this.inf = 'NEW';
-    } else if (notify.type === NotifyType.ALLOWANCE) {
-      this.img = 'assets/images/icon/allowance.png';
-      this.inf = '+';
-    } else if (notify.type === NotifyType.ADMIN) {
-      this.img = 'assets/images/tokens/tmp.png';
-      this.inf = '!!!';
-    } else if (notify.type === NotifyType.MEME_COIN) {
-      this.img = 'assets/images/icon/memecoin.png';
-      this.inf = notify.data;
+    }
+
+    if (this.img == null) {
+      if (notify.type === NotifyType.TOKEN) {
+        this.img = this.tokenIcons[notify.data];
+      } else if (notify.type === NotifyType.CREATING) {
+        this.img = this.avatarApi.dowloadAvatar(notify.data);
+      } else {
+        this.img = 'assets/images/tokens/tmp.png';
+      }
     }
   }
 
