8666d0fd781b7712dddf471d9a5a12496a96ff82
toxin <yuri.belousov.1998@gmail.com>
2019-12-04 17:15:08 +0300

    f particles donaters

--- src/app/shared/donater-rating/donater-rating.component.ts
+++ src/app/shared/donater-rating/donater-rating.component.ts
@@ -78,98 +78,114 @@ export class DonaterRatingComponent implements OnInit {
     };
 
     this.myParams = {
-      particles: {
-        number: {
-          value: 30,
-          density: {
-            enable: true,
-            value_area: 700
+      'particles': {
+        'number': {
+          'value': 69,
+          'density': {
+            'enable': true,
+            'value_area': 500
           }
         },
-        color: {
-          value: '#ffffff'
+        'color': {
+          'value': ['#268D3E', '#2675F6', '#7745C8', '#D19705', '#910809']
         },
-        shape: {
-          type: 'circle',
-          stroke: {
-            width: 10,
-            color: '#ffffff'
+        'shape': {
+          'type': 'circle',
+            'stroke': {
+            'width': 0,
+              'color': '#fff'
+          },
+          'polygon': {
+            'nb_sides': 5
+          },
+          'image': {
+            'src': 'img/github.svg',
+              'width': 100,
+              'height': 100
           }
         },
-        opacity: {
-          value: 0.5,
-          random: false,
-          anim: {
-            enable: false,
-            speed: 3,
-            opacity_min: 0.1,
-            sync: false
+        'opacity': {
+          'value': 1,
+            'random': true,
+            'anim': {
+            'enable': true,
+              'speed': 0.3,
+              'opacity_min': 0,
+              'sync': false
           }
         },
-        size: {
-          value: 5,
-          anim: {
-            enable: false,
-            speed: 40,
-            size_min: 0.1,
-            sync: false
+        'size': {
+          'value': 42,
+            'random': true,
+            'anim': {
+            'enable': true,
+              'speed': 2,
+              'size_min': 5,
+              'sync': false
           }
         },
-        line_linked: {
-          enable: true,
-          distance: 150,
-          color: '#ffffff',
-          opacity: 0.5,
-          width: 5
+        'line_linked': {
+          'enable': false,
+            'distance': 150,
+            'color': '#ffffff',
+            'opacity': 0.4,
+            'width': 1
         },
-        move: {
-          enable: true,
-          speed: 2,
-          direction: 'none',
-          random: false,
-          straight: false,
-          out_mode: 'out',
-          bounce: false,
-          attract: {
-            enable: false,
-            rotateX: 600,
-            rotateY: 1200
+        'move': {
+          'enable': true,
+            'speed': 1,
+            'direction': 'top',
+            'random': true,
+            'straight': false,
+            'out_mode': 'out',
+            'bounce': false,
+            'attract': {
+            'enable': false,
+              'rotateX': 600,
+              'rotateY': 600
           }
         }
       },
-      interactivity: {
-        detect_on: 'canvas',
-        events: {
-          onhover: {
-            enable: true,
-            mode: 'grab'
+      'interactivity': {
+        'detect_on': 'canvas',
+          'events': {
+          'onhover': {
+            'enable': false,
+              'mode': 'bubble'
           },
-          onclick: {
-            enable: true,
-            mode: 'push'
+          'onclick': {
+            'enable': false,
+              'mode': 'repulse'
           },
-          resize: true
+          'resize': true
         },
-        modes: {
-          grab: {
-            distance: 140,
-            line_linked: {
-              opacity: 1
+        'modes': {
+          'grab': {
+            'distance': 400,
+              'line_linked': {
+              'opacity': 1
             }
           },
-          repulse: {
-            distance: 200,
-            duration: 0.4
+          'bubble': {
+            'distance': 250,
+              'size': 0,
+              'duration': 2,
+              'opacity': 0,
+              'speed': 3
           },
-          push: {
-            particles_nb: 1
+          'repulse': {
+            'distance': 400,
+              'duration': 0.4
           },
-          remove: {
-            particles_nb: 2
+          'push': {
+            'particles_nb': 4
+          },
+          'remove': {
+            'particles_nb': 2
           }
         }
       },
-      retina_detect: true
+      'retina_detect': true
     };
   }
 }
