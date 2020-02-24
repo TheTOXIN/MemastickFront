133d5e740ff7c130c91c136bf21404a12a3d6009
TheTOXIN <thetoksin@gmail.com>
2020-02-24 20:10:30 +0300

    init laboratory

--- src/app/laboratory/laboratory.component.ts
+++ src/app/laboratory/laboratory.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import 'fabric';
+
+declare const fabric: any;
+
 @Component({
   selector: 'app-laboratory',
   templateUrl: './laboratory.component.html',
@@ -7,9 +11,536 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LaboratoryComponent implements OnInit {
 
-  constructor() { }
+  private canvas: any;
 
-  ngOnInit() {
+  private props: any = {
+    canvasFill: '#ffffff',
+    canvasImage: '',
+    id: null,
+    opacity: null,
+    fill: null,
+    fontSize: null,
+    lineHeight: null,
+    charSpacing: null,
+    fontWeight: null,
+    fontStyle: null,
+    textAlign: null,
+    fontFamily: null,
+    TextDecoration: ''
+  };
+
+  private textString: string;
+  private url = '';
+  private size: any = {
+    width: 500,
+    height: 800
+  };
+
+  private globalEditor = false;
+  private textEditor = false;
+  private imageEditor = false;
+  private figureEditor = false;
+  private selected: any;
+
+  constructor() {
   }
 
+  ngOnInit() {
+
+    // setup front side canvas
+    this.canvas = new fabric.Canvas('canvas', {
+      hoverCursor: 'pointer',
+      selection: true,
+      selectionBorderColor: 'blue'
+    });
+
+    this.canvas.on({
+      'object:moving': (e) => {
+      },
+      'object:modified': (e) => {
+      },
+      'object:selected': (e) => {
+
+        const selectedObject = e.target;
+        this.selected = selectedObject;
+        selectedObject.hasRotatingPoint = true;
+        selectedObject.transparentCorners = false;
+        // selectedObject.cornerColor = 'rgba(255, 87, 34, 0.7)';
+
+        this.resetPanels();
+
+        if (selectedObject.type !== 'group' && selectedObject) {
+
+          this.getId();
+          this.getOpacity();
+
+          switch (selectedObject.type) {
+            case 'rect':
+            case 'circle':
+            case 'triangle':
+              this.figureEditor = true;
+              this.getFill();
+              break;
+            case 'i-text':
+              this.textEditor = true;
+              this.getLineHeight();
+              this.getCharSpacing();
+              this.getBold();
+              this.getFontStyle();
+              this.getFill();
+              this.getTextDecoration();
+              this.getTextAlign();
+              this.getFontFamily();
+              break;
+            case 'image':
+              console.log('image');
+              break;
+          }
+        }
+      },
+      'selection:cleared': (e) => {
+        this.selected = null;
+        this.resetPanels();
+      }
+    });
+
+    this.canvas.setWidth(this.size.width);
+    this.canvas.setHeight(this.size.height);
+
+    // get references to the html canvas element & its context
+    // this.canvas.on('mouse:down', (e) => {
+    // let canvasElement: any = document.getElementById('canvas');
+    // console.log(canvasElement)
+    // });
+
+  }
+
+  /*------------------------Block elements------------------------*/
+
+  // Block "Size"
+
+  changeSize() {
+    this.canvas.setWidth(this.size.width);
+    this.canvas.setHeight(this.size.height);
+  }
+
+  // Block "Add text"
+
+  addText() {
+    const textString = this.textString;
+    const text = new fabric.IText(textString, {
+      left: 10,
+      top: 10,
+      fontFamily: 'helvetica',
+      angle: 0,
+      fill: '#000000',
+      scaleX: 0.5,
+      scaleY: 0.5,
+      fontWeight: '',
+      hasRotatingPoint: true
+    });
+    this.extend(text, this.randomId());
+    this.canvas.add(text);
+    this.selectItemAfterAdded(text);
+    this.textString = '';
+  }
+
+  // Block "Add images"
+
+  getImgPolaroid(event: any) {
+    const el = event.target;
+    fabric.Image.fromURL(el.src, (image) => {
+      image.set({
+        left: 10,
+        top: 10,
+        angle: 0,
+        padding: 10,
+        cornersize: 10,
+        hasRotatingPoint: true,
+        peloas: 12
+      });
+      image.setWidth(150);
+      image.setHeight(150);
+      this.extend(image, this.randomId());
+      this.canvas.add(image);
+      this.selectItemAfterAdded(image);
+    });
+  }
+
+  // Block "Upload Image"
+
+  addImageOnCanvas(url) {
+    if (url) {
+      fabric.Image.fromURL(url, (image) => {
+        image.set({
+          left: 10,
+          top: 10,
+          angle: 0,
+          padding: 10,
+          cornersize: 10,
+          hasRotatingPoint: true
+        });
+        image.setWidth(200);
+        image.setHeight(200);
+        this.extend(image, this.randomId());
+        this.canvas.add(image);
+        this.selectItemAfterAdded(image);
+      });
+    }
+  }
+
+  readUrl(event) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (eventLoad) => {
+        this.url = eventLoad.target['result'];
+      };
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  }
+
+  removeWhite(url) {
+    this.url = '';
+  }
+
+  // Block "Add figure"
+
+  addFigure(figure) {
+    let add: any;
+    switch (figure) {
+      case 'rectangle':
+        add = new fabric.Rect({
+          width: 200, height: 100, left: 10, top: 10, angle: 0,
+          fill: '#3f51b5'
+        });
+        break;
+      case 'square':
+        add = new fabric.Rect({
+          width: 100, height: 100, left: 10, top: 10, angle: 0,
+          fill: '#4caf50'
+        });
+        break;
+      case 'triangle':
+        add = new fabric.Triangle({
+          width: 100, height: 100, left: 10, top: 10, fill: '#2196f3'
+        });
+        break;
+      case 'circle':
+        add = new fabric.Circle({
+          radius: 50, left: 10, top: 10, fill: '#ff5722'
+        });
+        break;
+    }
+    this.extend(add, this.randomId());
+    this.canvas.add(add);
+    this.selectItemAfterAdded(add);
+  }
+
+  /*Canvas*/
+
+  cleanSelect() {
+    this.canvas.deactivateAllWithDispatch().renderAll();
+  }
+
+  selectItemAfterAdded(obj) {
+    this.canvas.deactivateAllWithDispatch().renderAll();
+    this.canvas.setActiveObject(obj);
+  }
+
+  setCanvasFill() {
+    if (!this.props.canvasImage) {
+      this.canvas.backgroundColor = this.props.canvasFill;
+      this.canvas.renderAll();
+    }
+  }
+
+  extend(obj, id) {
+    obj.toObject = (function (toObject) {
+      return function () {
+        return fabric.util.object.extend(toObject.call(this), {
+          id: id
+        });
+      };
+    })(obj.toObject);
+  }
+
+  setCanvasImage() {
+    const self = this;
+    if (this.props.canvasImage) {
+      this.canvas.setBackgroundColor({source: this.props.canvasImage, repeat: 'repeat'}, function () {
+        // self.props.canvasFill = '';
+        self.canvas.renderAll();
+      });
+    }
+  }
+
+  randomId() {
+    return Math.floor(Math.random() * 999999) + 1;
+  }
+
+  /*------------------------Global actions for element------------------------*/
+
+  getActiveStyle(styleName, object) {
+    object = object || this.canvas.getActiveObject();
+    if (!object) {
+      return '';
+    }
+
+    return (object.getSelectionStyles && object.isEditing)
+      ? (object.getSelectionStyles()[styleName] || '')
+      : (object[styleName] || '');
+  }
+
+
+  setActiveStyle(styleName, value, object) {
+    object = object || this.canvas.getActiveObject();
+    if (!object) {
+      return;
+    }
+
+    if (object.setSelectionStyles && object.isEditing) {
+      const style = {};
+      style[styleName] = value;
+      object.setSelectionStyles(style);
+      object.setCoords();
+    } else {
+      object.set(styleName, value);
+    }
+
+    object.setCoords();
+    this.canvas.renderAll();
+  }
+
+
+  getActiveProp(name) {
+    const object = this.canvas.getActiveObject();
+    if (!object) {
+      return '';
+    }
+
+    return object[name] || '';
+  }
+
+  setActiveProp(name, value) {
+    const object = this.canvas.getActiveObject();
+    if (!object) {
+      return;
+    }
+    object.set(name, value).setCoords();
+    this.canvas.renderAll();
+  }
+
+  clone() {
+    const activeObject = this.canvas.getActiveObject(), activeGroup = this.canvas.getActiveGroup();
+
+    if (activeObject) {
+      let clone;
+      switch (activeObject.type) {
+        case 'rect':
+          clone = new fabric.Rect(activeObject.toObject());
+          break;
+        case 'circle':
+          clone = new fabric.Circle(activeObject.toObject());
+          break;
+        case 'triangle':
+          clone = new fabric.Triangle(activeObject.toObject());
+          break;
+        case 'i-text':
+          clone = new fabric.IText('', activeObject.toObject());
+          break;
+        case 'image':
+          clone = fabric.util.object.clone(activeObject);
+          break;
+      }
+      if (clone) {
+        clone.set({left: 10, top: 10});
+        this.canvas.add(clone);
+        this.selectItemAfterAdded(clone);
+      }
+    }
+  }
+
+  getId() {
+    this.props.id = this.canvas.getActiveObject().toObject().id;
+  }
+
+  setId() {
+    const val = this.props.id;
+    const complete = this.canvas.getActiveObject().toObject();
+    console.log(complete);
+    this.canvas.getActiveObject().toObject = () => {
+      complete.id = val;
+      return complete;
+    };
+  }
+
+  getOpacity() {
+    this.props.opacity = this.getActiveStyle('opacity', null) * 100;
+  }
+
+  setOpacity() {
+    this.setActiveStyle('opacity', parseInt(this.props.opacity) / 100, null);
+  }
+
+  getFill() {
+    this.props.fill = this.getActiveStyle('fill', null);
+  }
+
+  setFill() {
+    this.setActiveStyle('fill', this.props.fill, null);
+  }
+
+  getLineHeight() {
+    this.props.lineHeight = this.getActiveStyle('lineHeight', null);
+  }
+
+  setLineHeight() {
+    this.setActiveStyle('lineHeight', parseFloat(this.props.lineHeight), null);
+  }
+
+  getCharSpacing() {
+    this.props.charSpacing = this.getActiveStyle('charSpacing', null);
+  }
+
+  setCharSpacing() {
+    this.setActiveStyle('charSpacing', this.props.charSpacing, null);
+  }
+
+  getFontSize() {
+    this.props.fontSize = this.getActiveStyle('fontSize', null);
+  }
+
+  setFontSize() {
+    this.setActiveStyle('fontSize', parseInt(this.props.fontSize), null);
+  }
+
+  getBold() {
+    this.props.fontWeight = this.getActiveStyle('fontWeight', null);
+  }
+
+  setBold() {
+    this.props.fontWeight = !this.props.fontWeight;
+    this.setActiveStyle('fontWeight', this.props.fontWeight ? 'bold' : '', null);
+  }
+
+  getFontStyle() {
+    this.props.fontStyle = this.getActiveStyle('fontStyle', null);
+  }
+
+  setFontStyle() {
+    this.props.fontStyle = !this.props.fontStyle;
+    this.setActiveStyle('fontStyle', this.props.fontStyle ? 'italic' : '', null);
+  }
+
+  getTextDecoration() {
+    this.props.TextDecoration = this.getActiveStyle('textDecoration', null);
+  }
+
+  setTextDecoration(value) {
+    let iclass = this.props.TextDecoration;
+    if (iclass.includes(value)) {
+      iclass = iclass.replace(RegExp(value, 'g'), '');
+    } else {
+      iclass += ` ${value}`;
+    }
+    this.props.TextDecoration = iclass;
+    this.setActiveStyle('textDecoration', this.props.TextDecoration, null);
+  }
+
+  hasTextDecoration(value) {
+    return this.props.TextDecoration.includes(value);
+  }
+
+  getTextAlign() {
+    this.props.textAlign = this.getActiveProp('textAlign');
+  }
+
+  setTextAlign(value) {
+    this.props.textAlign = value;
+    this.setActiveProp('textAlign', this.props.textAlign);
+  }
+
+  getFontFamily() {
+    this.props.fontFamily = this.getActiveProp('fontFamily');
+  }
+
+  setFontFamily() {
+    this.setActiveProp('fontFamily', this.props.fontFamily);
+  }
+
+  /*System*/
+
+  removeSelected() {
+    const activeObject = this.canvas.getActiveObject(),
+      activeGroup = this.canvas.getActiveGroup();
+
+    if (activeObject) {
+      this.canvas.remove(activeObject);
+      // this.textString = '';
+    } else if (activeGroup) {
+      const objectsInGroup = activeGroup.getObjects();
+      this.canvas.discardActiveGroup();
+      const self = this;
+      objectsInGroup.forEach(function (object) {
+        self.canvas.remove(object);
+      });
+    }
+  }
+
+  bringToFront() {
+    const activeObject = this.canvas.getActiveObject(),
+      activeGroup = this.canvas.getActiveGroup();
+
+    if (activeObject) {
+      activeObject.bringToFront();
+      // activeObject.opacity = 1;
+    } else if (activeGroup) {
+      const objectsInGroup = activeGroup.getObjects();
+      this.canvas.discardActiveGroup();
+      objectsInGroup.forEach((object) => {
+        object.bringToFront();
+      });
+    }
+  }
+
+  sendToBack() {
+    const activeObject = this.canvas.getActiveObject(),
+      activeGroup = this.canvas.getActiveGroup();
+
+    if (activeObject) {
+      activeObject.sendToBack();
+      // activeObject.opacity = 1;
+    } else if (activeGroup) {
+      const objectsInGroup = activeGroup.getObjects();
+      this.canvas.discardActiveGroup();
+      objectsInGroup.forEach((object) => {
+        object.sendToBack();
+      });
+    }
+  }
+
+  confirmClear() {
+    if (confirm('Are you sure?')) {
+      this.canvas.clear();
+    }
+  }
+
+  rasterize() {
+    if (!fabric.Canvas.supports('toDataURL')) {
+      alert('This browser doesn\'t provide means to serialize canvas to an image');
+    } else {
+      console.log(this.canvas.toDataURL('png'));
+      // window.open(this.canvas.toDataURL('png'));
+      const image = new Image();
+      image.src = this.canvas.toDataURL('png');
+      const w = window.open('');
+      w.document.write(image.outerHTML);
+    }
+  }
+
+  resetPanels() {
+    this.textEditor = false;
+    this.imageEditor = false;
+    this.figureEditor = false;
+  }
 }
--- src/app/laboratory/laboratory.module.ts
+++ src/app/laboratory/laboratory.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {LaboratoryComponent} from './laboratory.component';
+import {ColorPickerModule} from 'ngx-color-picker';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     LaboratoryComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ColorPickerModule
   ]
 })
 export class LaboratoryModule { }
