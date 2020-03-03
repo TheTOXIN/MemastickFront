ffb427db46259be66628effda9031cc3af83acad
toxin <yuri.belousov.1998@gmail.com>
2020-03-03 19:05:58 +0300

    more rank data

--- src/app/model/MemetickRank.ts
+++ src/app/model/MemetickRank.ts
@@ -2,6 +2,9 @@ export class MemetickRank {
   constructor(
     public dna: number,
     public lvl: number,
+    public next: number,
+    public left: number,
+    public percent: number,
     public name: string,
   ) {
 
