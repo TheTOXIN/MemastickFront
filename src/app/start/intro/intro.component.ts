0d96d23c83d2863103d9c8d47e33122c8b155575
TheTOXIN <thetoksin@gmail.com>
2020-05-10 00:22:19 +0300

    + buty start

--- src/app/start/intro/intro.component.ts
+++ src/app/start/intro/intro.component.ts
@@ -10,12 +10,52 @@ import {OauthApiService} from '../../services/oauth-api-service';
 })
 export class IntroComponent implements OnInit {
 
+  private messagesOne = [
+    'Хааааай',
+    'Когда релиз?',
+    'Как тебе мой мем?',
+    '10010001001',
+    'Что думаешь?',
+    'Где новые мемы?',
+    'А я покакал кста',
+    'Одэмен петух',
+    'Есть тут кто?',
+    'Сиди дома дурак',
+  ];
+
+  private messagesTwo = [
+    'Не важно',
+    'Нормас )0))',
+    'А ты кто такой?',
+    'Кааавооо',
+    'Осуждаю такое',
+    'Я хочу питсы',
+    'У меня лапки',
+    'Зачем ты так',
+    'Аахаххааах',
+    'Надеюсь на лучшее',
+  ];
+
+  public firstMessage = '';
+  public secondMessage = '';
+
   constructor(
     private router: Router
   ) {
+    this.initmessages();
+    this.initTimer();
   }
 
   ngOnInit() {
+  }
+
+  initTimer() {
+    setInterval(() => this.initmessages(), 3000);
+  }
+
+  initmessages() {
+    this.firstMessage = this.messagesOne[Math.floor(Math.random() * this.messagesOne.length)];
+    this.secondMessage = this.messagesTwo[Math.floor(Math.random() * this.messagesTwo.length)];
   }
 
   toIvite() {
