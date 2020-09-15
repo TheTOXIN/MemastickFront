7308b18767eec9a7d387c2e36004ffaab82c1532
TheTOXIN <thetoksin@gmail.com>
2020-09-15 17:53:21 +0300

    f card drag

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,30 +1,48 @@
 import {Component, ComponentFactoryResolver, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
 import {CardState} from '../../state/card-state.service';
-import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {animate, keyframes, style, transition, trigger} from '@angular/animations';
+
+const slideInUp  = [
+  style({visibility: 'visible', transform: 'translate3d(0, 100%, 0)'}),
+  style({transform: 'translate3d(0, 0, 0)'})
+];
+
+const slideOutDown  = [
+  style({transform: 'translate3d(0, 0, 0)'}),
+  style({visibility: 'hidden', transform: 'translate3d(0, 100%, 0)'})
+];
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  animations: [
+    trigger('cardAnim', [
+      transition('* => open', animate(228, keyframes(slideInUp))),
+      transition('* => close', animate(228, keyframes(slideOutDown)))
+    ])
+  ]
 })
 export class CardComponent implements OnInit {
 
   @ViewChild('content', {read: ViewContainerRef}) ctr: ViewContainerRef;
 
+  animState = '';
+
   constructor(
-    private state: CardState,
+    private cardState: CardState,
     private resolve: ComponentFactoryResolver
   ) {
-
+    this.animState = 'open';
   }
 
   ngOnInit() {
-    const content = this.state.options.content;
+    const content = this.cardState.options.content;
 
     const factory = this.resolve.resolveComponentFactory(content);
     const ref: any = this.ctr.createComponent(factory);
 
-    ref.instance.options = this.state.options;
+    ref.instance.options = this.cardState.options;
 
     if (ref.instance.closer != null) {
       ref.instance.closer.subscribe(() => {
@@ -33,7 +51,17 @@ export class CardComponent implements OnInit {
     }
   }
 
+  startClose() {
+    this.animState = 'close';
+  }
+
+  endClose() {
+    if (this.animState === 'close') {
+      this.close();
+    }
+  }
+
   close() {
-    this.state.modal.close();
+    this.cardState.modal.close();
   }
 }
