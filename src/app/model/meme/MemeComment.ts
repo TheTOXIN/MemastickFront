b95bad6b29b1e484daf489df609865d9a2be6092
TheTOXIN <thetoksin@gmail.com>
2020-09-16 12:29:22 +0300

    + comment nick

--- src/app/model/meme/MemeComment.ts
+++ src/app/model/meme/MemeComment.ts
@@ -3,7 +3,9 @@ import {UUID} from 'angular2-uuid';
 export class MemeComment {
   constructor(
     public commentId: UUID,
+    public memeId: UUID,
     public memetickId: UUID,
+    public nick: string,
     public comment: string,
     public point: number,
     public vote: Boolean
