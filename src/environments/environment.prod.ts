c7e68292b21359dbe5a5df21b1d02b61e6db4f43
TheTOXIN <thetoksin@gmail.com>
2020-09-22 21:17:51 +0300

    DURAK

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -12,8 +12,8 @@ export const environment = {
   backUrl: 'https://memastick-back.herokuapp.com',
   frontUrl: 'https://www.memastick.ru',
   androidUrl: 'android-app://xyz.appmaker.qkufdm',
-  accessTokenTime: 3600,
-  refreshTokenTime: 2592000,
+  accessTokenTime: 15,
+  refreshTokenTime: 60,
   vkChat: true,
   metrika: true
 };
